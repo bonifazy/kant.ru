398ed2feb72b0eccd4407c2ef9f719983d7c00d1
Dim March <dkmarchuk@gmail.com>
2021-07-15 23:30:32 +0300

    add sql to work with models to logger/views.py

--- logger/views.py
+++ logger/views.py
@@ -1,20 +1,48 @@
 from django.shortcuts import render
-from .models import Products, Prices, InstockNagornaya
+# from .models import Products, Prices, InstockNagornaya
+
+"""
+# Самые продаваемые модели бренда Asics, первые 20 шт.
+SELECT DISTINCT p.code, p.brand, p.model, i.rating 
+FROM instock_nagornaya AS i 
+JOIN products AS p
+ON code_id=code
+WHERE p.brand="Asics"
+ORDER BY i.rating DESC
+LIMIT 20;
+
+# Актуальный размерный ряд модели по коду товара с учетом рейтинга, 
+  даже если при обновленном состоянии размера нет в наличии
+SELECT  p.code, p.brand, p.model, i.size, i.rating
+FROM instock_nagornaya AS i
+JOIN products AS p
+ON code=code_id
+WHERE p.code=1599734
+GROUP BY i.size
+HAVING MAX(i.rating) AND i.count<>0;
+
+# Приоритет выставления брендов в пагинатор, относительно суммы рейтингов последних обновлений наличия
+SELECT SUM(rate)
+FROM (
+    SELECT i.rating as rate
+    FROM instock_nagornaya AS i 
+    JOIN products AS p
+    ON code_id=code
+    WHERE p.brand="Adidas"
+    ORDER BY -i.rating
+    LIMIT 20
+    );
+
+# Актуальная стоимость товара
+SELECT price
+FROM prices 
+WHERE code_id="1270586"
+ORDER BY rating DESC
+LIMIT 1;
+"""
 
 
 def index(request):
-    """
-    shoes = Products.objects.all()[:50]
-    # add props
-    for shoe in shoes:
-        price_obj = Prices.objects.get(code_id=shoe.code)
-        shoe.price, shoe.price_time, shoe.price_rate = price_obj.price, price_obj.timestamp, price_obj.rating
-        nag = InstockNagornaya.objects.all().order_by('-rating')
-        shoe.size, shoe.count, shoe.nag_time, shoe.nag_rate = nag.count, nag.timestamp, nag.rating
-    """
     templates = 'display/index_with_js.html'
-    context = dict()
-    #    'shoes': shoes,
-    #}
 
-    return render(request, templates, context)
+    return render(request, templates, dict())
