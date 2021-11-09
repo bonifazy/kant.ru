858965ef7a4ad9fda8c2a4800068ea2df38e3074
Dim March <dkmarchuk@gmail.com>
2021-11-09 16:10:57 +0300

    prepare_to_prod

--- kant/settings.py
+++ kant/settings.py
@@ -4,21 +4,18 @@ import logger.apps
 BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PARENT = Path(__file__).resolve().parent.parent.parent.joinpath('data/')
 DB_FILE = DB_PARENT.joinpath('db.sqlite3')
+PROJECT_ROOT = BASE_DIR
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ''  # in file and env var
-
-# from django.core.management.utils import get_random_secret_key
 with open('.django_secret') as f:
     SECRET_KEY = f.read().strip()
 
-# ALLOWED_HOSTS = ['134.209.88.84']  # to production
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '134.209.88.84']  # to prod, to dev
+DEBUG = True  # True- dev, False- prod
 
-# Application definition
+if DEBUG:  # dev
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+else:  # prod
+    ALLOWED_HOSTS = ['marchdim.dev', 'www.marchdim.dev', '134.209.88.84', 'localhost']
+    
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -43,6 +40,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'kant.urls'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('static/')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -63,20 +63,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kant.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': DB_FILE,
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,44 +85,24 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'ru'
-
 TIME_ZONE = 'Europe/Moscow'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-PROJECT_ROOT = BASE_DIR
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath(STATIC_URL)
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Deploy
+if DEBUG:  # dev
+    SECURE_HSTS_SECONDS = 600
+    SECURE_SSL_REDIRECT = False
+else:  # prod
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_SSL_REDIRECT = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # to production
-# DEBUG = True  # to development
+#SECURE_CONTENT_TYPE_NOSNIFF = True
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#SESSION_COOKIE_SECURE = True
+#SECURE_HSTS_PRELOAD = True
+#CSRF_COOKIE_SECURE = True
 
-SECURE_HSTS_SECONDS = 31536000  # prod
-# SECURE_HSTS_SECONDS = 600  # dev
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-SECURE_HSTS_PRELOAD = True
-CSRF_COOKIE_SECURE = True
